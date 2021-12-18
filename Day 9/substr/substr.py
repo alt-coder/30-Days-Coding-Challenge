d7e076b57f4ed97de7e566aad59c6bcd7bc9d92e
Shubham Soni <nandcmos@gmail.com>
2021-12-18 09:28:41 +0530

    Day 11

--- Day 9/substr/substr.py
+++ Day 9/substr/substr.py
@@ -2,7 +2,7 @@ def substr(s,length):
     '''all substring function based on length'''
     if (length == 0):
         return
-    substr(s,length-1)
+    substr(s,length-1)# decrement the length and print those
     for i in range(len(s)-length+1):
         print(s[i:i+length])
     
@@ -12,9 +12,24 @@ def substr_start(s,start):
         return
 
     for i in range(start+1,len(s)+1):
-        
-        if start+1 == i :substr_start(s,i)
         print(s[start:i])
-s="abcde"
-substr(s,5)
+        if start+1 == i :substr_start(s,i)
+        #print(s[start:i])
+def subs(s,ans,start,i):
+    '''printing substing based on inclusion exclusion
+    start : index of next char to be appended to the ans
+    i: index of last character appended to the ans'''
+    if start == len(s):
+        print(ans)
+        return
+    
+    if(i==-1 or start-i <= 1):subs(s,ans+s[start],start+1,start)#to make sure that we print substring not sub sequence
+    else:#if we have ever skiped a character(except skipping initial chars) then we shall never contiue further
+        print(ans)
+        return 
+    subs(s,ans,start+1,i) #dont take a charter
+s="abcd"
+# substr(s,5)
 substr_start(s,0)
+print()
+subs(s,"",0,-1)
---
+++ Day11/OneByNthrootBinarySearch.py
@@ -0,0 +1,17 @@
+def find(num,n):
+    low=0.01
+    high=num
+    
+    while (high-low >=0.001): #limit the precision
+        test = low+((high-low)/2)
+        if test == low or test == high:
+            return test         #sometimes due to precision they can become equal so break
+        val = test**n
+        if val > num:
+            high=test
+        elif val < num:
+            low= test
+        else:
+            return test
+    return test+0.0005
+print(find(53,3))
